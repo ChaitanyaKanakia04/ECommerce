d9f0be1bee9cf5617afaa48f71ae27a5f3930686
ChaitanyaKanakia04 <chaitanya.kanakia98@gmail.com>
2021-11-03 19:28:33 +1100

    Creating file structure

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+        <div>
+            <h1 className="myName">Chaitanya Kanakia</h1>
+            <p>I am an international student, studying masters of information technology at monash university. I am keen on becoming a web developer and learning new skills. This is my attempt at learning react from scratch.</p>
+            <ul>
+                <li>Bora Bora</li>
+                <li>Switzerland</li>
+                <li>Iceland</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//Styling
 import "./index.css";
 
-function MyInfo() {
-    return (
-        <div>
-            <h1 className="myName">Chaitanya Kanakia</h1>
-            <p>I am an international student, studying masters of information technology at monash university. I am keen on becoming a web developer and learning new skills. This is my attempt at learning react from scratch.</p>
-            <ul>
-                <li>Bora Bora</li>
-                <li>Switzerland</li>
-                <li>Iceland</li>
-            </ul>
-        </div>
-    )
-}
+//Components
+import MyInfo from "./components/MyInfo"
 
 ReactDOM.render(
     <MyInfo />,
